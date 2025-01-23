1982754caecc66dbaf2f1b0c62397a686fc2f750
Admin <Admin@DESKTOP-N2G5C2S>
2025-01-23 20:17:33 +0100

    Update_23-01-2025_20:17

--- app/user.py
+++ app/user.py
@@ -1,19 +1,41 @@
 import hashlib
-class UserRepo:
-    def __init__(self, username: str, password: str):
-        self._username = None
-        self._hashed_password = None
 
-        if username:
-            self._username = username
+from flask import Flask, render_template, request, redirect, url_for
+class LoginApi:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.add_routes()
 
-        if password:
-            self._hashed_password = hashlib.sha512(password.encode()).hexdigest()
+    def run(self, *args, **kwargs):
+        self.app.run(*args, **kwargs)
 
-    def login(self, login: str, password: str):
-        hash = hashlib.sha512(password.encode())
+    def add_routes(self):
+        self.app.add_url_rule('/', 'login', self.login, methods=['GET', 'POST'])
+        self.app.add_url_rule('/success', 'success', self.success, methods=['GET'])
+        self.app.add_url_rule('/failure', 'failure', self.failure, methods=['GET'])
 
-        if self._username == login and self._hashed_password == hash.hexdigest():
-            return True
-        else:
-            return False
+    def login(self):
+        if request.method == "POST":
+            username = request.form.get("username")
+            password = request.form.get("password")
+
+            print(f'Username: {username} | Password: {password}')
+
+            if username == "Admin" and password == "Admin":
+                return redirect(url_for('success'))
+            else:
+                return redirect(url_for('failure'))
+
+        return render_template("login.html")
+
+    def success(self):
+        # return render_template("vue_success.html")
+        return render_template("success.html")
+
+    def failure(self):
+        return render_template("failure.html")
+
+
+if __name__ == "__main__":
+    app = LoginApi()
+    app.run(debug=False, port=5000)
