94e2f0438ffb57171514b80974fa1b2e4b676f07
Bartek <sedbar12@gmail.com>
2025-01-24 18:47:29 +0100

    Working mark 24-01-2025-18:46

--- app/mark.py
+++ app/mark.py
@@ -8,10 +8,10 @@ class Mark(db.Model):
     description = db.Column(db.String(255),nullable=True)
 
     student_id = db.Column(db.Integer, db.ForeignKey('users.id'),nullable=False)
-    #grup_id = db.Column(db.Integer, db.ForeignKey('groups.id'), nullable=False)
+    group_id = db.Column(db.Integer, db.ForeignKey('groups.id'), nullable=False)
 
     student = db.relationship('Student', back_populates='marks')
-    #group = db.relationship('Group', back_populates='marks')
+    group = db.relationship('Group', back_populates='marks')
 
     def __repr__(self):
         return f"<Mark(value={self.value}, description='{self.description}')>"
--- app/student.py
+++ app/student.py
@@ -18,10 +18,11 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         student = Student.query.filter_by(username="Janekthebuilder").first()
-        #group = GroupRepo.find_by_argument(name="Beginner English")
+        group_repo = GroupRepo()
+        group = group_repo.find_by_argument(name="Beginner English")
         # Dodaj nową ocenę
-        mark1 = Mark(value=4.5, description="Egzamin końcowy", student=student)
-        mark2 = Mark(value=5.0, description="Sprawdzian z algebry", student=student)
+        mark1 = Mark(value=4.5, description="Egzamin końcowy", student=student, group=group)
+        mark2 = Mark(value=5.0, description="Sprawdzian z algebry", student=student, group=group)
         db.session.add(mark1)
         db.session.add(mark2)
         db.session.commit()
