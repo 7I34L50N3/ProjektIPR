19d32c4ffb613bae02a06fef7b8e66a636ea9ef5
Julian670 <j.zebrecki@gmail.com>
2025-01-25 21:22:52 +0100

    Update 25-01-2025_21:20

--- app/api.py
+++ app/api.py
@@ -228,7 +228,7 @@ class GroupApi:
     def register_routes(self, app):
         app.add_url_rule('/groups', 'groups', self.groups, methods=['GET'])
         app.add_url_rule('/edit_group', 'edit_group', self.edit, methods=['POST'])
-        app.add_url_rule('/add_group', 'add_group', self.add_group, methods=['POST'])
+        app.add_url_rule('/add_group', 'add_group', self.add, methods=['POST'])
 
     def groups(self):
         user_id = session.get('user_id')
@@ -237,31 +237,31 @@ class GroupApi:
             return redirect(url_for('login'))
 
         groups_data = {
-            "groups": [
-                {
-                    "id": 1,
-                    "group_id": "021_WT",
-                    "language": "Niemiecki",
-                    "teacher": "Adolf H.",
-                    "teacher_id": 1,  # ID nauczyciela
-                    "schedule": "Śr. 15:40",
-                    "student_ids": [1, 2],
-                    "students": ["Wanda", "Karolina"]
-                }
-            ],
-            "all_students": [
-                {"id": 1, "first_name": "Wanda", "last_name": "Narkiewicz"},
-                {"id": 2, "first_name": "Karolina", "last_name": "Nowak"},
-                {"id": 3, "first_name": "Waldek", "last_name": "Kowalski"},
-                {"id": 4, "first_name": "Jędrzej", "last_name": "Bąk"}
-            ],
-            "all_teachers": [
-                {"id": 1, "first_name": "Adolf", "last_name": "H."},
-                {"id": 2, "first_name": "Anna", "last_name": "Kowalska"},
-                {"id": 3, "first_name": "Jan", "last_name": "Nowak"}
-            ],
-            "all_schedules": ["Pn. 9:00", "Śr. 15:40", "Pt. 18:00"],
-            "all_languages": ["Niemiecki", "Angielski", "Francuski", "Hiszpański"]
+            # "groups": [
+            #     {
+            #         "id": 1,
+            #         "group_id": "021_WT",
+            #         "language": "Niemiecki",
+            #         "teacher": "Adolf H.",
+            #         "teacher_id": 1,  # ID nauczyciela
+            #         "schedule": "Śr. 15:40",
+            #         "student_ids": [1, 2],
+            #         "students": ["Wanda", "Karolina"]
+            #     }
+            # ],
+            # "all_students": [
+            #     {"id": 1, "first_name": "Wanda", "last_name": "Narkiewicz"},
+            #     {"id": 2, "first_name": "Karolina", "last_name": "Nowak"},
+            #     {"id": 3, "first_name": "Waldek", "last_name": "Kowalski"},
+            #     {"id": 4, "first_name": "Jędrzej", "last_name": "Bąk"}
+            # ],
+            # "all_teachers": [
+            #     {"id": 1, "first_name": "Adolf", "last_name": "H."},
+            #     {"id": 2, "first_name": "Anna", "last_name": "Kowalska"},
+            #     {"id": 3, "first_name": "Jan", "last_name": "Nowak"}
+            # ],
+            # "all_schedules": ["Pn. 9:00", "Śr. 15:40", "Pt. 18:00"],
+            # "all_languages": ["Niemiecki", "Angielski", "Francuski", "Hiszpański"]
         }
         return render_template("groups.html", **groups_data)
 
