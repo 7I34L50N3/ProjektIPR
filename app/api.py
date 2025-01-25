219d163d61adb74d9862cdc5ff417ae67ec2b770
Julian670 <j.zebrecki@gmail.com>
2025-01-25 21:27:53 +0100

    Update 25-01-2025_21:20

--- app/api.py
+++ app/api.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 from user import UserRepo, User
 from student import Student
-from group import Group
+from group import Group,GroupRepo
 from admin import Admin
 from globals import app, db
 from hashlib import sha256
@@ -236,32 +236,36 @@ class GroupApi:
             flash("Musisz być zalogowany, aby uzyskać dostęp do tej strony.", "error")
             return redirect(url_for('login'))
 
+        group_repo = GroupRepo()
+        all_groups = group_repo.find()
         groups_data = {
-            # "groups": [
-            #     {
-            #         "id": 1,
-            #         "group_id": "021_WT",
-            #         "language": "Niemiecki",
-            #         "teacher": "Adolf H.",
-            #         "teacher_id": 1,  # ID nauczyciela
-            #         "schedule": "Śr. 15:40",
-            #         "student_ids": [1, 2],
-            #         "students": ["Wanda", "Karolina"]
-            #     }
-            # ],
-            # "all_students": [
-            #     {"id": 1, "first_name": "Wanda", "last_name": "Narkiewicz"},
-            #     {"id": 2, "first_name": "Karolina", "last_name": "Nowak"},
-            #     {"id": 3, "first_name": "Waldek", "last_name": "Kowalski"},
-            #     {"id": 4, "first_name": "Jędrzej", "last_name": "Bąk"}
-            # ],
-            # "all_teachers": [
-            #     {"id": 1, "first_name": "Adolf", "last_name": "H."},
-            #     {"id": 2, "first_name": "Anna", "last_name": "Kowalska"},
-            #     {"id": 3, "first_name": "Jan", "last_name": "Nowak"}
-            # ],
-            # "all_schedules": ["Pn. 9:00", "Śr. 15:40", "Pt. 18:00"],
-            # "all_languages": ["Niemiecki", "Angielski", "Francuski", "Hiszpański"]
+            #"groups": [Group.check_info_group() for Group in all_groups]}
+
+            "groups": [
+                {
+                    "id": 1,
+                    "group_id": "021_WT",
+                    "language": "Niemiecki",
+                    "teacher": "Adolf H.",
+                    "teacher_id": 1,  # ID nauczyciela
+                    "schedule": "Śr. 15:40",
+                    "student_ids": [1, 2],
+                    "students": ["Wanda", "Karolina"]
+                }
+            ],
+            "all_students": [
+                {"id": 1, "first_name": "Wanda", "last_name": "Narkiewicz"},
+                {"id": 2, "first_name": "Karolina", "last_name": "Nowak"},
+                {"id": 3, "first_name": "Waldek", "last_name": "Kowalski"},
+                {"id": 4, "first_name": "Jędrzej", "last_name": "Bąk"}
+            ],
+            "all_teachers": [
+                {"id": 1, "first_name": "Adolf", "last_name": "H."},
+                {"id": 2, "first_name": "Anna", "last_name": "Kowalska"},
+                {"id": 3, "first_name": "Jan", "last_name": "Nowak"}
+            ],
+            "all_schedules": ["Pn. 9:00", "Śr. 15:40", "Pt. 18:00"],
+            "all_languages": ["Niemiecki", "Angielski", "Francuski", "Hiszpański"]
         }
         return render_template("groups.html", **groups_data)
 
