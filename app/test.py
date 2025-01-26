98c8d4f152a1a04908e868f274f67f681fc0d36c
Bartek <sedbar12@gmail.com>
2025-01-26 21:26:43 +0100

    update 26-01-2025 21:25

--- app/test.py
+++ app/test.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
-
+from user import UserRepo, user
 import logging
 
 logging.basicConfig(level=logging.INFO)
